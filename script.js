5b878c8208b9ee6027732819df2455b672e2d91f
reinismel <reinis.melgailis@gmail.com>
2022-03-08 11:14:37 +0200

    darbs

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const vardi = ["Ezis","Dzenis","Zirgs","Ali Džahameds","Kastanis"]
-const balvas = ["Švalbes perm","Draudzeni","M4A4","Aladina paklājs","Divas kinderolas"]
-const naudaKopa = 1000000
-let uzvaretajuSkaits = 5
+const vardi = ["Ezis","Dzenis","Zirgs","Džahomahameds","Kastanis"];
+const balvas = ["Švalbes perm","Draudzeni","M4A4","Aladina paklājs","Divas kinderolas"];
+let uzvaretajuSkaits = 5 ;
+let rindas= document.querySelector('.rindas');//pievieno mainigo
 
-Math.random=Math.random.apply()*10
-rand=Math.floor(rand) //noapaļo uz leju
-console.log(vardi[rand]);
+
+//let rand= Math.random()*vardi.length;
+//Math.random=Math.random()*10;
+//rand=Math.floor(rand); //noapaļo uz leju
+//console.log(vardi[rand]);
+
+for (let i=0;i<uzvaretajuSkaits;i++){
+    let rand= Math.random()*vardi.length;
+    rand=Math.floor(rand); //noapaļo uz leju
+    let uzvaretajs = vardi[rand];//izvada konsolē
+    //let uzvaretajs = vardi[rand];
+    rindas.innerHTML+=//pievieno +, lai pieskaititu nakamo
+}

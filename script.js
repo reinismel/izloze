f6bec99d90162d6b33c69c9c83a31413d49959f3
reinismel <reinis.melgailis@gmail.com>
2022-03-08 10:24:08 +0200

    pirmais

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const balvas = ["Švalbes perm","Draudzeni","M4A4","Aladina paklājs","Divas kin
 const naudaKopa = 1000000
 let uzvaretajuSkaits = 5
 
+Math.random=Math.random.apply()*10
+rand=Math.floor(rand) //noapaļo uz leju
 console.log(vardi[rand]);

b71e912fa27b276f45ecbf01a715ebc6f4c09ab1
reinismel <reinis.melgailis@gmail.com>
2022-03-08 13:15:49 +0200

    adada

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const vardi = ["Ezis","Dzenis","Zirgs","Džahomahameds","Kastanis"];
 const balvas = ["Švalbes perm","Draudzeni","M4A4","Aladina paklājs","Divas kinderolas"];
 let uzvaretajuSkaits = 5 ;
 let rindas= document.querySelector('.rindas');//pievieno mainigo
-
+rindas.innerHTML = ` `;
 
 //let rand= Math.random()*vardi.length;
 //Math.random=Math.random()*10;
@@ -14,10 +14,16 @@ for (let i=0;i<uzvaretajuSkaits;i++){
     rand=Math.floor(rand); //noapaļo uz leju
     let uzvaretajs = vardi[rand];//izvada konsolē
     //let uzvaretajs = vardi[rand];
+    let laimests = balvas[rand];
     rindas.innerHTML+=`   
     <tr>
         <td>${i+1}</td>
         <td>${uzvaretajs}</td>
+        <td>${laimests}</td>
         </tr>`
 
 }
+
+
+
+

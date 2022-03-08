3b75167d5cca070065e4c7fa1d03ae88dff8b177
reinismel <reinis.melgailis@gmail.com>
2022-03-08 10:18:52 +0200

    pirmais

---
+++ script.js
@@ -0,0 +1,6 @@
+const vardi = ["Ezis","Dzenis","Zirgs","Ali Džahameds","Kastanis"]
+const balvas = ["Švalbes perm","Draudzeni","M4A4","Aladina paklājs","Divas kinderolas"]
+const naudaKopa = 1000000
+let uzvaretajuSkaits = 5
+
+console.log(vardi[rand]);
